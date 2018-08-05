d111478995be15e16141890e7a4ebd3eda045106
NemoZhang <am1122@163.com>
2018-08-06 00:01:28 +0800

    Initial commit

---
+++ conf/setting.py
@@ -0,0 +1,34 @@
+import os
+import pymysql
+
+
+API_URL = 'http://hn2.api.okayapi.com/'
+
+# 路径配置
+BASE_PATH = os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__)))
+# excel测试用例存放位置
+CASE_PATH = os.path.join(BASE_PATH, 'data')
+# unittest测试用例文件存放位置
+TEST_PATH = os.path.join(BASE_PATH, 'testcases')
+# 测试报告路径
+REPORT_PATH = os.path.join(BASE_PATH, 'report')
+
+# 测试时自动查找excel用例文件的通配符
+FILE_NAME = ''
+
+# 服务相关配置
+APP_KEY = 'DA8ED0D9F1D522934AFCB6552A45DE02'
+APP_SECRET = 'DBQjXjmZRwmgaaWPtm66xpV1enCPkZcxfxWyS2Hh5ewZvSLC7PV0uJyo2uGZkKgOVOFxsbR'
+
+# 数据库配置
+# SQL_CONFIG = {
+#     'host': '127.0.0.1',
+#     'port': 3306,
+#     'user': 'root',
+#     'password': 'root',
+#     'db': 'ecshop',
+#     'charset': 'utf8mb4',
+#     'cursorclass': pymysql.cursors.DictCursor  # 加上这个参数会将查询结果变为[{"字段名":"字段值",...},{}]的形式
+# }
+SQL_CONFIG = 'D:\PythonProject\Demo\myflask\db.db'
---
+++ lib/utils.py
@@ -0,0 +1,62 @@
+import hashlib
+import sqlite3
+from conf.setting import *
+
+
+def set_request_data(params, key=None, secret=None):
+    """
+    根据小白接口的规则生成签名
+    :param params: 请求中的所有参数
+    :param key:
+    :param secret:
+    :return:
+    """
+    if not params:
+        params = {}
+    key = key or APP_KEY
+    secret = secret or APP_SECRET
+    params.pop('app_secret', None)
+    params['app_key'] = key
+    params_str = (''.join([str(params[value]) for value in sorted([key for key in params])])
+                  + secret).encode('utf-8')
+    md5_ctx = hashlib.md5()
+    md5_ctx.update(params_str)
+    params['sign'] = md5_ctx.hexdigest().upper()
+    return params
+
+
+def set_md5(pwd):
+    """
+    通过MD5加密传输的密码
+    :param pwd:
+    :return:
+    """
+    md5 = hashlib.md5()
+    if pwd:
+        md5.update(pwd.encode('utf-8'))
+        return md5.hexdigest().upper()
+    else:
+        return
+
+
+def set_res_data(res):
+    """
+    返回的报文是json格式的，利用字符替换让数据变成"a=1,b=2"的格式
+    :param res: Response的文本数据r.text
+    :return:
+    """
+    return res.replace('":"', '=').replace('":', '=')
+
+
+def get_data_by_sql(sql, *args):
+    conn = sqlite3.connect(SQL_CONFIG)
+    cur = conn.cursor()
+    return cur.execute(sql, args).fetchone()[0]
+
+
+def get_datas_by_sql(sql, *args):
+    conn = sqlite3.connect(SQL_CONFIG)
+    cur = conn.cursor()
+    return cur.execute(sql, args).fetchall()
+
+
